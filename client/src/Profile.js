ab054a5b67f5a8491c2f4f46c115f4464a6b8e90
npage1129 <82468860+npage1129@users.noreply.github.com>
2022-05-30 14:10:33 -0600

    patch in progress

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,24 +1,55 @@
 import React,{useState} from 'react';
 import Header from './Header';
+import Add from './Add';
 import './Profile.css';
 import AddIcon from '@mui/icons-material/Add';
 import {useNavigate} from 'react-router'
 import HomeIcon from '@mui/icons-material/Home';
 
-function Profile({posts,user,}) {
+function Profile({posts,user}) {
+
     const [show, setShow]= useState('false')
+    const [userCaption, setUserCaption] = useState([]);
+    const [updatedCaption, setUpdatedCaption] = useState("");
+    const [id, setId] = useState(0)
   const navigate = useNavigate()
   function showButton(){
     setShow(!show)
   }
+  function handleUpdateCaption(updatedCaption) {
+    const updatedPostsArray = userCaption.map((capt) => {
+      if (capt.id === updatedCaption.id) {
+        return updatedCaption;
+      } else {
+        return capt;
+      }
+    });
+   setUserCaption(updatedPostsArray);
+  }
+  function handlePriceFormSubmit(e) {
+    e.preventDefault();
+    fetch(`http://localhost:3000/posts/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ updatedCaption }),
+    })
+      .then((r) => r.json())
+      .then((updatedCaption) => {
+        handleUpdateCaption(updatedCaption);
+      });
+  }
+ 
   const postIt =  posts.filter(post => post.user.id === user.id)
   console.log(posts)
   return (
     <div className="middle">
-      <div className="home">
+      <Add/>
+      <div className="home">  
       <HomeIcon onClick={()=>{navigate('/home')}}/>
-      <AddIcon  onClick={()=>{navigate('/add')}}/>
   </div>
+
 <br></br>
 <br></br>
 <br></br>
@@ -34,7 +65,8 @@ function Profile({posts,user,}) {
                   <div>
                     <img className = "post_image" src ={posts.img} key = {posts.id}/> 
                     <div className = "captions">
-                    {posts.caption} 
+                    {posts.caption} <input placeholder ="Update Caption" value={updatedCaption} 
+                    onChange={(e)=> setUpdatedCaption(e.target.value)}></input>
               
                     </div>
                     {show? (posts.comments.map((comment)=>
